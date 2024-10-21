db77cfd49db6e5d5bd59521618d8043440eb0faf
AkbarF0rce <akbarridhoarrafi@gmail.com>
2024-10-21 12:27:14 +0700

    minor changes

--- src/dashboard/dashboard.service.ts
+++ src/dashboard/dashboard.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { CreateDashboardDto } from './dto/create-dashboard.dto';
-import { UpdateDashboardDto } from './dto/update-dashboard.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Topic } from 'src/topic/entities/topic.entity';
 import { Repository } from 'typeorm';
@@ -82,7 +80,6 @@ export class DashboardService {
       .select('EXTRACT(MONTH FROM transaction.created_at) as month')
       .addSelect('SUM(transaction.total) as total_income')
       .where('EXTRACT(YEAR FROM transaction.created_at) = :year', { year })
-      .andWhere('transaction.status = :status', { status: 'success' })
       .groupBy('month')
       .orderBy('month', 'ASC')
       .getRawMany();
@@ -103,12 +100,11 @@ export class DashboardService {
   async totalIncome() {
     const count = await this.transactionsRepository
       .createQueryBuilder('transaction')
-      .where('transaction.status = :status', { status: 'success' })
       .select('SUM(transaction.total) as total_income')
       .getRawOne();
 
     return {
-      total: count.total_income,
+      total: parseInt(count.total_income),
     };
   }
 }
